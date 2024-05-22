d21c1e247c444d3f5155167885cce28bd40f44d7
= <=>
2024-05-22 12:55:27 +0530

    restframe work change in seetings.py

--- Social_Media/settings.py
+++ Social_Media/settings.py
@@ -37,8 +37,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "rest_framework",
     "Social_Media_App",
-    "restframework",
 ]
 
 MIDDLEWARE = [
