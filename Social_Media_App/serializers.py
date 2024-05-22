741420dd7f18357939042d7753e8447c5a0fd630
= <=>
2024-05-22 18:40:37 +0530

    added checkpassword

--- Social_Media_App/serializers.py
+++ Social_Media_App/serializers.py
@@ -2,12 +2,17 @@
 
 from rest_framework import serializers
 from .models import user_signup
+from django.contrib.auth.hashers import make_password
 
 
 class UserSignupSerializer(serializers.ModelSerializer):
     class Meta:
         model = user_signup
         fields = ["fullname", "username", "email", "password"]
+
+    def create(self, validated_data):
+        validated_data["password"] = make_password(validated_data["password"])
+        return super(UserSignupSerializer, self).create(validated_data)
 
 
 class UserLoginSerializer(serializers.Serializer):
