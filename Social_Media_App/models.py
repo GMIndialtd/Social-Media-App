ab37ac441cf76f23e5e6bb74bb6f5b0486bf3997
= <=>
2024-05-22 15:44:27 +0530

    user class is changed

--- Social_Media_App/models.py
+++ Social_Media_App/models.py
@@ -1,14 +1,14 @@
 from django.db import models
 
+
 # Create your models here.
-class signup(models.Model):
+class user_signup(models.Model):
     fullname = models.CharField(max_length=255)
-    username = models.CharField(max_length=50, unique=True, null=False, blank=False )
-    email = models.EmailField(max_length=100, unique=True,null=False, blank=False)
-    password = models.CharField(max_length=255,null=False, blank=False)
-    created_on = models.DateTimeField(auto_now_add=True,null=False, blank=False)
-    updated_on = models.DateTimeField(auto_now=True,null=False, blank=False)
+    username = models.CharField(max_length=50, unique=True, null=False, blank=False)
+    email = models.EmailField(max_length=100, unique=True, null=False, blank=False)
+    password = models.CharField(max_length=255, null=False, blank=False)
+    created_on = models.DateTimeField(auto_now_add=True, null=False, blank=False)
+    updated_on = models.DateTimeField(auto_now=True, null=False, blank=False)
 
     def __str__(self):
         return self.username
-    
