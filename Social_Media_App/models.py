ab3fb1e6d16304e35568efa5bbfe2b01bfc8ce5e
= <=>
2024-05-24 12:54:41 +0530

    models snakecode

--- Social_Media_App/models.py
+++ Social_Media_App/models.py
@@ -2,9 +2,6 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.core.validators import RegexValidator
 
-# from django.contrib.auth.models import User
-
-
 # Create your models here.
 class CustomUser(AbstractUser):
     email = models.EmailField(max_length=100, unique=True, null=False, blank=False)
